9f5fe0149a25449c8075c468312c485fb91d06e5
LackFos <LackFosisOnline@gmail.com>
2025-08-29 20:03:47 +0700

    Show username instead of ID

--- background.js
+++ background.js
@@ -7,7 +7,7 @@ chrome.history.onVisited.addListener((historyItem) => {
   if (match) {
     const userId = match[1];
 
-    chrome.storage.local.get([url.host], (result) => {
+    chrome.storage.local.get([url.host], async (result) => {
       const histories = result[url.host] ?? [];
 
       const isHistoryExist = histories.find((history) => history.userId === userId);
@@ -15,8 +15,16 @@ chrome.history.onVisited.addListener((historyItem) => {
 
       if (histories.length >= 5) histories.shift();
 
+      const request = await fetch(`${url.origin}/dashboard`, { credentials: "include" });
+      const body = await request.text();
+      const match = body.match(/<div class="d-none d-xl-block ps-2">\s*<div>(.*?)<\/div>/);
+      const username = match ? match[1].trim() : "Error";
+
       chrome.storage.local.set({
-        [url.host]: [...histories, { userId: userId, url: historyItem.url, visited_at: Date.now() }],
+        [url.host]: [
+          ...histories,
+          { userId: userId, username: username, url: historyItem.url, visited_at: Date.now() },
+        ],
       });
     });
   }
--- popup/popup.js
+++ popup/popup.js
@@ -22,7 +22,7 @@ document.addEventListener("DOMContentLoaded", () => {
         });
 
         return `
-          <div data-url="${history.url}" class="card card--row">
+          <div data-url="${history.url}" class="card card--row card--shadow">
             <div class="avatar">
               <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M20 21C20 18.2386 16.4183 16 12 16C7.58172 16 4 18.2386 4 21M12 13C9.23858 13 7 10.7614 7 8C7 5.23858 9.23858 3 12 3C14.7614 3 17 5.23858 17 8C17 10.7614 14.7614 13 12 13Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
@@ -30,7 +30,7 @@ document.addEventListener("DOMContentLoaded", () => {
             </div>
 
             <div class="card__content">
-              <div class="card__title">${history.userId}</div>
+              <div class="card__title">${history.username}</div>
               <div class="card__description">${formattedVisitedAt}</div>
             </div>
           </div>
@@ -43,6 +43,7 @@ document.addEventListener("DOMContentLoaded", () => {
       $main.querySelectorAll("[data-url]").forEach(($card) => {
         $card.addEventListener("click", async () => {
           await fetch(`${url.origin}/stop-impersonating`);
+
           const targetUrl = $card.getAttribute("data-url");
           chrome.tabs.create({ url: targetUrl });
         });
